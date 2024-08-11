d1df57c8489a60897929412f86a61210d41ce5cb
Monsieur <christopher.chhim123@gmail.com>
2024-08-10 23:26:01 -0700

    Updated models and routes

--- models/index.js
+++ models/index.js
@@ -5,13 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  through: {
+    model: Tag,
+    unique: flase
+  },
+})
 // Categories have many Products
-
+Category.haveMany(Product, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+})
 module.exports = {
   Product,
   Category,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll();
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Location.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'product_category'}]
+    })
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
